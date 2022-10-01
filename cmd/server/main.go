14872870c392370cd0708bc8b5618369bd753056
by-sabbir <me@sabbir.dev>
2022-10-01 16:29:20 +0600

    chore: db initialized and migrated

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,25 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+
+	"github.com/by-sabbir/company-microservice-rest/pkg/db"
+)
 
 func Run() error {
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to database")
+		return err
+	}
+	log.Println("Successfully Connected to the DB!")
 
+	if err := db.MigrateDB(); err != nil {
+		return fmt.Errorf("migrations failed because of: %w", err)
+	}
+
+	// cmpService := company.NewService(db)
 	return nil
 }
 
